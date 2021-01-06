9c8970b9dd6b6a3830a92a052867aa7614b9a83d
Ronhoho <ande1218@gmail.com>
2021-01-06 18:13:14 +0800

    Configure CartService to use HttpClient.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -9,15 +10,18 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductChartComponent } from './product-chart/product-chart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'product/:productId', component: ProductDetailsComponent },
       { path: 'chart', component: ProductChartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -26,7 +30,8 @@ import { ProductChartComponent } from './product-chart/product-chart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    ProductChartComponent
+    ProductChartComponent,
+    ShippingComponent
   ],
   bootstrap: [ AppComponent ]
 })
